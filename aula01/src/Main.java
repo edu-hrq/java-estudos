e470c7cf0a40c209531bd7fd54b2b86897415ce5
edu-hrq <eduardohenriquedasilva1096@gmail.com>
2026-08-04 21:54:14 -0300

    Aula 01: Tipos de variáveis

--- aula01/src/Main.java
+++ aula01/src/Main.java
@@ -2,7 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Olá, mundo!");
+        // Tipos de Variáveis:
+
+        String nome = "Eduardo";
+        int idade = 21;
+        double altura = 1.90;
+        double peso = 120.50;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura + "m");
+        System.out.println("Peso: " + peso + "kg");
 
     }
 
